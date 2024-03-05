522806f2056bf75463e7b6fda856923c7cea3ae6
Lucas da Cunha <luques.cunha@gmail.com>
2024-03-05 16:24:08 -0300

    direitos autorais

--- components/footer.js
+++ components/footer.js
@@ -96,9 +96,24 @@ export default function Footer() {
         </div>
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-          Copyright © {new Date().getFullYear()}.
+          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
+          <a
+            href="https://web3templates.com/"
+            target="_blank"
+            rel="noopener">
+            Web3Templates.
+          </a>{" "}
+          Illustrations from{" "}
+          <a
+            href="https://www.glazestock.com/"
+            target="_blank"
+            rel="noopener ">
+            Glazestock
+          </a>
         </div>
       </Container>
+      {/* Do not remove this */}
+      <Backlink />
     </div>
   );
 }

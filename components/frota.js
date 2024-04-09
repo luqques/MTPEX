91b927096b9993b62f04a4aa1c06258651d4e340
Lucas da Cunha <lucas.cunha@ailos.coop.br>
2024-04-09 15:45:37 -0300

    fazendo carousel 3d

--- components/frota.js
+++ components/frota.js
@@ -1,48 +1,44 @@
-import React, { useState } from "react";
-import Image from "next/image";
 import Container from "./container";
-//import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
 import SectionTitle from "./sectionTitle";
+import React from "react";
+import Card from "./card.js";
+import Carrossel from "./carrossel.js"
+import Image from "next/image";
 import c4Lounge from "../public/img/c4-lounge.jpg";
 
-const CARDS = 10;
-const MAX_VISIBILITY = 3;
-
-const Card = ({ title, content }) => {
-    <div className="card">
-        <h2>{title}</h2>
-        <p>{content}</p>
-        <Image src={c4Lounge} alt="Imagem do carro" width={300} height={200} />
-    </div>
-};
-
-const Carousel = ({ children }) => {
-    const [active, setActive] = useState(2);
-    const count = React.Children.count(children);
-
-    return (
-        <div className="carousel">
-            {active > 0 && <button className="nav left" onClick={() =>
-                setActive(i => i - 1)}> seta esquerda </button>}
-            {React.Children.map(children, (child, i) => (
-                <div className="card-container" style={{
-                    '--active': i == active ? 1 : 0,
-                    '--offset': (active - i) / 3,
-                    '--direction': Math.sign(active - i),
-                    '--abs-offset': Math.abs(active - i) / 3,
-                    'pointer-events': active === i ? 'auto' : 'none',
-                    'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
-                    'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
-                }}>
-                    {child}
-                </div>
-            ))}
-            {active < count - 1 && <button className="nav right" onClick={() => setActive(i => 1)}> seta direita </button>}
-        </div>
-    );
-};
-
-const Frota = () => {
+function Frota() {
+    let cards = [
+      {
+        key: uuidv4(),
+        content: (
+          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
+        )
+      },
+      {
+        key: uuidv4(),
+        content: (
+          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+        )
+      },
+      {
+        key: uuidv4(),
+        content: (
+          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+        )
+      },
+      {
+        key: uuidv4(),
+        content: (
+          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
+        )
+      },
+      {
+        key: uuidv4(),
+        content: (
+          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
+        )
+      }
+    ];
     return (
         <Container>
             <SectionTitle
@@ -50,17 +46,18 @@ const Frota = () => {
                 title="Frota de Veículos">
                 Todos os carros disponíveis para atendimento, garantindo qualidade, conforto e segurança para qualquer tipo de viagem.
             </SectionTitle>
-            <Carousel>
-                {[...new Array(CARDS)].map((_, i) => (
-                    <Card 
-                        key={i}
-                        title={'Card ' + (i + 1)}
-                        content='teste de card'
-                    />
-                ))}
-            </Carousel>
+            <div className="">
+                <Carrossel
+                cards={cards}
+                height="500px"
+                width="30%"
+                margin="0 auto"
+                offset={2}
+                showArrows={false}
+                />
+            </div>
         </Container>
-    )
-}
+    );
+  }
 
 export default Frota;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
-import "../css/styles.css";
 
 function MyApp({ Component, pageProps }) {
   return (

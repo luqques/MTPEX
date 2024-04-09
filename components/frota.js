5f892ac2e30f1f01bc533849705bb26ae4287a7f
Lucas da Cunha <luques.cunha@gmail.com>
2024-04-09 17:14:43 -0300

    inclusão do uuid

--- components/frota.js
+++ components/frota.js
@@ -1,43 +1,44 @@
 import Container from "./container";
 import SectionTitle from "./sectionTitle";
 import React from "react";
-import Card from "./card.js";
-import Carrossel from "./carrossel.js"
+import Card from "./card";
+import Carrossel from "./carrossel"
+import { v4 as uuidv4 } from "uuid";
 import Image from "next/image";
 import c4Lounge from "../public/img/c4-lounge.jpg";
 
 function Frota() {
     let cards = [
-      {
-        key: uuidv4(),
-        content: (
-          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
-        )
-      },
-      {
-        key: uuidv4(),
-        content: (
-          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
-        )
-      },
-      {
-        key: uuidv4(),
-        content: (
-          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
-        )
-      },
-      {
-        key: uuidv4(),
-        content: (
-          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
-        )
-      },
-      {
-        key: uuidv4(),
-        content: (
-          <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
-        )
-      }
+        {
+            key: uuidv4(),
+            content: (
+                <Card imagem="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
+            )
+        },
+        {
+            key: uuidv4(),
+            content: (
+                <Card imagem="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+            )
+        },
+        {
+            key: uuidv4(),
+            content: (
+                <Card imagem="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+            )
+        },
+        {
+            key: uuidv4(),
+            content: (
+                <Card imagem="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
+            )
+        },
+        {
+            key: uuidv4(),
+            content: (
+                <Card imagem="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
+            )
+        }
     ];
     return (
         <Container>
@@ -48,16 +49,16 @@ function Frota() {
             </SectionTitle>
             <div className="">
                 <Carrossel
-                cards={cards}
-                height="500px"
-                width="30%"
-                margin="0 auto"
-                offset={2}
-                showArrows={false}
+                    cards={cards}
+                    height="500px"
+                    width="30%"
+                    margin="0 auto"
+                    offset={2}
+                    showArrows={false}
                 />
             </div>
         </Container>
     );
-  }
+}
 
 export default Frota;
